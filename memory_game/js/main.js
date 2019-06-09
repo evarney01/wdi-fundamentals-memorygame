538fdbfa703e494a26c3ade4a66c43d60c8e5777
Eric <eric.varney@yahoo.com>
2019-06-09 14:41:20 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,13 +3,24 @@ console.log("Up and running!");
 let cards = ['queen', 'queen', 'king', 'king']
 let cardsInPlay = []
 
+funtion.checkForMatch({
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!")} 
+    else ({
+        console.log("Sorry, try again.");
+)}
+
+
+function.flipCard({
+	if (cardsInPlay[0] === cardsInPlay[1]) { 
+	    console.log("You found a match!")}
+    else ({ cardsInPlay[0] !== cardsInPlay[1]) 
+	    alert("Sorry, try again.!")}
+
 let cardOne = cards[0]
 cardsInPlay.push(cardOne)
 let cardTwo = cards[2]
 cardsInPlay.push(cardTwo)
 console.log(cardsInPlay)
 //  there should be two cards in the card array`
-if (cardsInPlay[0] === cardsInPlay[1]) { 
-	console.log("You found a match!")}
-else cardsInPlay[0] !== cardsInPlay[1] 
-	alert("Sorry, try again.!");
+
