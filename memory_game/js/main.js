bfcaa3f4736d263d67eb19815c64b863c407e52c
Eric <eric.varney@yahoo.com>
2019-06-09 16:07:44 -0500

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,63 @@
 console.log("Up and running!");
 
-let cards = ['queen', 'queen', 'king', 'king']
+let cards = [{
+	var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  },
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+}]
 let cardsInPlay = []
 
-funtion.checkForMatch({
+checkForMatch({
 	if (cardsInPlay[0] === cardsInPlay[1]) {
         console.log("You found a match!")} 
     else ({
         console.log("Sorry, try again.");
 )}
 
-
-function.flipCard({
+flipCard({
 	if (cardsInPlay[0] === cardsInPlay[1]) { 
 	    console.log("You found a match!")}
-    else ({ cardsInPlay[0] !== cardsInPlay[1]) 
-	    alert("Sorry, try again.!")}
+    else ({ cardsInPlay[0] !== cardsInPlay[1] }) 
+	    alert("Sorry, try again.!")
 
 let cardOne = cards[0]
 cardsInPlay.push(cardOne)
