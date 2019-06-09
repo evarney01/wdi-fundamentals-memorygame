cc44fd7934aed0b80d3f60a796aac854195cf262
Eric <eric.varney@yahoo.com>
2019-06-09 18:36:42 -0500

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,68 +1,37 @@
 console.log("Up and running!");
 
-let cards = [{
-	var cards = [
-  {
-    rank: "queen",
+let cards = [{rank: "queen",
     suit: "hearts",
-    cardImage: "images/queen-of-hearts.png"
-  },
-  {
-    rank: "queen",
+    cardImage:"images/queen-of-hearts.png" },{rank: "queen",
     suit: "diamonds",
-    cardImage: "images/queen-of-diamonds.png"
-  },
-  {
-    rank: "king",
+    cardImage:"images/queen-of-diamonds.png" },{rank: "king",
     suit: "hearts",
-    cardImage: "images/king-of-hearts.png"
-  },
-  {
-    rank: "king",
+    cardImage:"images/king-of-hearts.png" },{rank: "king",
     suit: "diamonds",
-    cardImage: "images/king-of-diamonds.png"
-  },
-  {
-    rank: "queen",
-    suit: "hearts",
-    cardImage: "images/queen-of-hearts.png"
-  },
-  {
-    rank: "queen",
-    suit: "diamonds",
-    cardImage: "images/queen-of-diamonds.png"
-  },
-  {
-    rank: "king",
-    suit: "hearts",
-    cardImage: "images/king-of-hearts.png"
-  },
-  {
-    rank: "king",
-    suit: "diamonds",
-    cardImage: "images/king-of-diamonds.png"
-  }
-];
-}]
+    cardImage:"images/king-of-diamonds.png" }]
+	
+function createBoard(){
+    for(let i = 0; i < cards.length; i++) {
+        cards[i].cardImage
+    }
+}
 let cardsInPlay = []
 
-checkForMatch({
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!")} 
-    else ({
-        console.log("Sorry, try again.");
-)}
 
-flipCard({
-	if (cardsInPlay[0] === cardsInPlay[1]) { 
-	    console.log("You found a match!")}
-    else ({ cardsInPlay[0] !== cardsInPlay[1] }) 
-	    alert("Sorry, try again.!")
 
-let cardOne = cards[0]
-cardsInPlay.push(cardOne)
-let cardTwo = cards[2]
-cardsInPlay.push(cardTwo)
-console.log(cardsInPlay)
-//  there should be two cards in the card array`
+function cardFlip(num){
+    
+    let flipCard = cards[num]
+    
 
+    cardsInPlay.push(flipCard)
+}
+
+
+
+function checkMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("you found a match")
+    } else {
+        console.log("No match")
+    };
